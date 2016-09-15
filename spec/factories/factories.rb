08e2aab288a5e2e7fcd71bd1d8e8ad26eef6eda8
Leonardo Bernardellli <leobernardelli@gmail.com>
2016-09-15 17:26:02 -0300

    fixing circular dependency on tests

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
 
-  factory :message_retry, class: Hovercat::MessageRetry do
+  factory :message_retry, class: 'Hovercat::MessageRetry' do
     payload {'teste payload'}
     header {'teste header'}
     routing_key {'teste routing key'}
