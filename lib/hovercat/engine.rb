8887891a688590ffed65002c63c21309373bb3ce
Thiago Assis <thiago.assis@m4u.com.br>
2017-05-23 18:44:07 -0300

    :bamboo: Re-adding generators

--- lib/hovercat/engine.rb
+++ lib/hovercat/engine.rb
@@ -3,5 +3,12 @@ module Hovercat
 
   class Engine < ::Rails::Engine
     isolate_namespace Hovercat
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => false
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.assets false
+      g.helper false
+    end
   end
 end
