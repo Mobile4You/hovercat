28d87f806d375d5a747c5b7cd6347ba758074fda
Thiago Assis <thiago.assis@m4u.com.br>
2017-05-23 18:40:26 -0300

    :bamboo: Changing migration import

--- lib/hovercat/engine.rb
+++ lib/hovercat/engine.rb
@@ -3,20 +3,5 @@ module Hovercat
 
   class Engine < ::Rails::Engine
     isolate_namespace Hovercat
-
-    config.generators do |g|
-      g.test_framework :rspec, :fixture => false
-      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
-      g.assets false
-      g.helper false
-    end
-
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths['db/migrate'].expanded.each do |expanded_path|
-          app.config.paths['db/migrate'] << expanded_path
-        end
-      end
-    end
   end
 end
