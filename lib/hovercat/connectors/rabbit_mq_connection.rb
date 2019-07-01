53de40df7fbfc6672c63a986c2fd05635db40458
Rogério Aguilar <rogerio.aguilar@m4u.com.br>
2019-07-01 20:47:15 -0300

    using singleton to create bunny connection

---
+++ lib/hovercat/connectors/rabbit_mq_connection.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+require 'bunny'
+require 'singleton'
+
+module Hovercat
+  module Connectors
+    class RabbitMQConnection
+      include Singleton
+      attr_reader :connection
+
+      def initialize
+        @connection = Bunny.new(bunny_params).start
+      end
+
+      def channel
+        @channel = @connection.create_channel unless @channel.open?
+
+        @channel
+      end
+
+      def self.reset!
+        @connection = nil
+      end
+
+      private
+
+      def bunny_params
+        {
+          hosts: Hovercat::CONFIG['hovercat']['rabbitmq']['host'],
+          port: Hovercat::CONFIG['hovercat']['rabbitmq']['port'],
+          vhost: Hovercat::CONFIG['hovercat']['rabbitmq']['vhost'],
+          user: Hovercat::CONFIG['hovercat']['rabbitmq']['user'],
+          password: Hovercat::CONFIG['hovercat']['rabbitmq']['password']
+        }
+      end
+    end
+  end
+end
